d9999c505e6314ff1c39598a695e1cbec943a714
AmiDholakiya <amidholakiya624@gmail.com>
2023-09-24 16:28:21 -0400

    feat:mongodb connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,29 @@
 from flask import Flask
 from flask_cors import CORS
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
 
 from config import Config
 import os
 
 
-NODE_DB = os.environ.get('NODE_DB')
-
 def create_app(config_class = Config):
+    load_dotenv()
     app = Flask(__name__)
     app.config.from_object(config_class)
     CORS(app)
 
+    # Configure Database
+    cluster  = MongoClient(os.getenv('MONGODB_URI'))
+    try:
+        cluster .admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    global db
+    db = cluster[os.getenv('MONGO_DB')]
+
+    # Register blueprint of application
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
@@ -20,6 +32,6 @@ def create_app(config_class = Config):
 
     @app.route('/test')
     def test_page():
-        return f'Hello, World! {NODE_DB}'
+        return f'Hello, World!'
     
-    return app;
+    return app
--- app/projects/routes.py
+++ app/projects/routes.py
@@ -1,5 +1,8 @@
-from app.projects import bp
+from pymongo.collection import Collection
 
+from app.projects import bp
+from app import db
+col: Collection = db['projectTB']
 @bp.route('/')
 def index():
     return "This is The Project Main Route"
