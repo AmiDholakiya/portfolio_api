4690f458313623623a0321b6e95921ea459257df
Amiben Dholakiya <amidholakiya624@gmail.com>
2023-10-15 20:54:08 -0400

    fix:production docs

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,7 @@ from pymongo.mongo_client import MongoClient
 
 def create_app():
     global app, db, s3, jwt_obj
-    app = FastAPI(
-        openapi_prefix="/prod"
-    )
+    app = FastAPI(openapi_prefix="/prod")
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
